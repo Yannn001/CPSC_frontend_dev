b09e5c1e76589a4cdcf1bfa9e4072ff0c1ad571f
chengyan <wang.12517@buckeyemail.osu.edu>
2022-09-29 14:31:16 -0400

    translated contact and event page

--- src/pages/NewHome/Contact.js
+++ src/pages/NewHome/Contact.js
@@ -8,9 +8,9 @@ export default function Contact() {
             <section id="contact">
                 <div class="container contact__container">
                     <div class="contact__left">
-                        <h2>Let's Talk</h2>
+                        <h2>联系我们</h2>
                         <p>
-                            Hi, thank you for visiting my website, if you would like to contact me, feel free to leave you name, email, and a short message here. I will get back to you as soon as possible.
+                            谢谢你浏览我们的网站, 如果你对我们的活动感兴趣的话, 不妨留下联系方式, 我们会主动与你取得联系.
                         </p>
                         <ul class="contact__socials">
                             <li>
--- src/pages/NewHome/Event.js
+++ src/pages/NewHome/Event.js
@@ -8,33 +8,33 @@ export default function Event() {
             <section id="portfolio">
       <div class="container portfolio__container">
         <div class="portfolio__head">
-          <h1>Portfolio</h1>
+          <h1>读书会活动</h1>
           <div class="portfolio__head-right">
             <p>
-              Here are some examples of my website
+              点击链接可以查看读书会安排哦
             </p>
-            <a href="https://github.com/Yannn001?tab=repositories">Explore More <i class="uil uil-arrow-right"></i></a>
+            <a href="https://github.com/Yannn001?tab=repositories">如何报名 <i class="uil uil-arrow-right"></i></a>
           </div>
         </div>
 
         <div class="portfolio__projects">
           <article class="portfolio__project">
             <span class="portfolio__project-image"
-              ><img src="./images/project1.png"
+              ><img src="https://meet.eslite.com/Content/Images/Article/1200x628_20220209143427.jpg"
             /></span>
             <div class="portfolio__cta">
               <a href="https://github.com/Yannn001/task_manager" class="btn" target="_blank"
-                >Github</a
+                >日程安排</a
               >
             </div>
           </article>
           <article class="portfolio__project">
             <span class="portfolio__project-image"
-              ><img src="./images/project2.png"
+              ><img src="https://user-images.githubusercontent.com/69735000/191848907-d4a0b1a0-a32d-4595-93f8-d688cd4966c5.jpg"
             /></span>
             <div class="portfolio__cta">
               <a href="https://github.com/Yannn001/CPCOH" class="btn" target="_blank"
-                >Github</a
+                >日程安排</a
               >
             </div>
           </article>
