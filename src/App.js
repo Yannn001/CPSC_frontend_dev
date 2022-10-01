dfad8ad01dfb18200e757e32d8a1f3f275da9376
Yannn001 <69735000+Yannn001@users.noreply.github.com>
2022-09-30 23:47:30 -0400

    added popup by Chengyan

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Signin from './pages/signin/Signin'
 import ChatRoom from './pages/chatroom/ChatRoom';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/NewHome/Home'
+import Popup from './pages/popup/Popup';
 
 
 
@@ -16,6 +17,7 @@ import Home from './pages/NewHome/Home'
           <Route path="/Signup" element={<Signup/>} />
           <Route path="/Signin" element={<Signin/>} />
           <Route path="/Chat" element={<ChatRoom/>} />
+          <Route path="/Test" element={<Popup/>} />
           <Route path="/" element={<Home/>} />
         </Routes>
     </div>
