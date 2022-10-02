76afe3defcbd3509b88c268e6992048652db55e0
Yannn001 <69735000+Yannn001@users.noreply.github.com>
2022-10-02 09:57:18 -0400

    .

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Signup from './pages/signup/Signup'
 import Signin from './pages/signin/Signin'
 import ChatRoom from './pages/chatroom/ChatRoom';
-import { Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/NewHome/Home'
 import Calender from './pages/NewHome/Calender'
 import Popup from './pages/popup/Popup';
